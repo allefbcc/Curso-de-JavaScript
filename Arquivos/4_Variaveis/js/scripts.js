62c830ac3a9f2b2cd0ffafeb7a263b6a4828d143
allef_bcc <allefbcc@gmail.com>
2023-05-17 10:41:38 -0300

    mais conteúdos de JavaScript

---
+++ Arquivos/4_Variaveis/js/scripts.js
@@ -0,0 +1,39 @@
+var teste = 1;
+
+var nome = "Állef"
+
+console.log(teste);
+
+teste = "Allen";
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'Állef';
+
+console.log(meuPrimeiroNome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = 'Állef'
+
+console.log(meuNome);
---
+++ Arquivos/8_Null_Undefined/js/scripts.js
@@ -0,0 +1,16 @@
+// Hoisting - içamento
+
+console.log(sobrenome);
+console.log(numero);
+
+var nome = null;
+var sobrenome = "Allen";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Állef";
+
+console.log(nome);
+
+var numero = 5;
